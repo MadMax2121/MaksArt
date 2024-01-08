cfdf435ee41ad3c372b5e4b031242cffd6a80691
max_moskalenko <mmoskalenko21@gmail.com>
2024-01-07 23:41:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import './App.css';
 import Header from './Header/Header.js';
-import FirstContainer from './FirstContainer/FirstContainer.js';
+import Slides from './Slides/Slides.js';
 import FirstThree from './FirstThree/FirstThree';
 import NewArrivals from './NewArrivals/NewArrivals';
 import FeaturedProducts from './FeaturedProducts/FeaturedProducts';
 import Footer from './Footer/Footer';
 
 
-// In your component
-
-
 function App() {
-  return (
-    <div className="app">
-        <div style={{"background-color":"rgb(248, 248, 252)", position: "fixed", "z-index":"3", width:"100%"}}>
-        <Header />
-        </div>
-        
-        <div style={{"background-color":"rgb(248, 248, 252)"}}>
-        <FirstContainer />
-        
-        
-        </div>
-        <div className='secondSection' style={{"background-color":"white"}}>
-        <FirstThree/>
-        <NewArrivals/>
-        </div>
-
-        <FeaturedProducts/>
-        <div></div>
-        <Footer/>
-        
-        
+ return (
+   <div className="app">
+       <div style={{"background-color":"white", "z-index":"3", width:"100%"}}>
+       <Header />
+       </div>
+      
+       <div style={{"background-color":"white"}}>
+       <Slides />
+      
+      
+       </div>
+       <div classN ame='secondSection' style={{"background-color":"white"}}>
+       <FirstThree/>
+       <NewArrivals/>
+       </div>
 
 
-    </div>
-  );
+       <FeaturedProducts/>
+       <div></div>
+       <Footer/>
+      
+      
+
+
+
+
+   </div>
+ );
 }
+
 
 export default App;
