e5721378cc57123b0e01473f9c5264256cf8053b
max_moskalenko <mmoskalenko21@gmail.com>
2024-01-08 16:39:17 -0500

    pages and transitions

--- src/App.js
+++ src/App.js
@@ -1,41 +1,67 @@
 import './App.css';
 import Header from './Header/Header.js';
-import Slides from './Slides/Slides.js';
-import NewArrivals from './NewArrivals/NewArrivals';
-import FeaturedProducts from './FeaturedProducts/FeaturedProducts';
-import Footer from './Footer/Footer';
 import Home from './Home';
 import About from './About';
 import Collection from './Collection';
 import Contact from './Contact';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 
 
+function AnimatedRoutes() {
+  const location = useLocation(); // Get the current location
 
+  const pageVariants = {
+    initialState: {
+      opacity: 0,
 
-function App() {
-return (
- <Router>
-  <div className="app">
-      <div style={{backgroundColor:"white", zIndex:"3", width:"100%"}}>
-      <Header />
-      </div>
-      <Routes>
-       <Route exact path= '/'  element={<Home/>}></Route>
-       <Route exact path= '/about'  element={<About/>}></Route>
-       <Route exact path= '/collection'  element={<Collection/>}></Route>
-       <Route exact path= '/contact'  element={<Contact/>}></Route>
+    },
+    in: {
+      opacity: 1,
+    },
+  };
 
+  
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route exact path='/' element={
+          <motion.div initial="initialState" animate="in" exit="out" variants={pageVariants} transition='0.2' >
+            <Home />
+          </motion.div>
+        } />
+        <Route exact path='/about' element={
+          <motion.div initial="initialState" animate="in" exit="out" variants={pageVariants}>
+            <About />
+          </motion.div>
+        } />
+        <Route exact path='/collection' element={
+          <motion.div initial="initialState" animate="in" exit="out" variants={pageVariants} >
+            <Collection />
+          </motion.div>
+        } />
+        <Route exact path='/contact' element={
+          <motion.div initial="initialState" animate="in" exit="out" variants={pageVariants} >
+            <Contact />
+          </motion.div>
+        } />
       </Routes>
-  </div>
-  </Router>
-);
+    </AnimatePresence>
+  );
 }
 
-
-
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <div style={{ backgroundColor: "white", zIndex: "3", width: "100%" }}>
+          <Header />
+        </div>
+        <AnimatedRoutes />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
-
-
-
