82813fd62c961e6530d2a15ece87a923607a4364
max_moskalenko <mmoskalenko21@gmail.com>
2024-01-08 13:21:13 -0500

    finished header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,30 +4,37 @@ import './Header.css'
 
 
 function Header() {
- return (
-   <main className="main">
+  return (
+    <main className="main">
 
 
-     <img className = "logo" src="/logo/logo.png" alt="" />
+      <img className="logo" src="/logo/logo.png" alt="" />
 
+      <div className="header">
 
-      <div className = "menu">
-       <input  className = "search_bar" type="text" name placeholder='Search for a product...'></input>
+        <div className='menu'>
+          <button>Home</button>
+          <button>About Us</button>
+          <button>Our Collection</button>
+          <button>Contact Us</button>
+        </div>
+        
+        <input className="search_bar" type="text" name placeholder='Search for a product...'></input>
       </div>
-  
-     <div className='icons'>
-       <button className="nav-icon">
-         <img className="search" src="/icons/search.png" alt="" />
-       </button>
-       <button className="nav-icon">
-         <img className="settings" src="/icons/settings.png" alt="" />
-       </button>
-       <button className="nav-icon">
-         <img className="cart" src="/icons/cart.png" alt="" />
-       </button>
-     </div>
-   </main>
- )
+
+      <div className='icons'>
+      <button className="nav-icon">
+          <img className="profile" src="/header/profile.svg" alt="" />
+        </button>
+        <button className="nav-icon">
+        <img className="cart" src="/header/cart.svg" alt="" />
+      
+        </button>
+       
+      
+      </div>
+    </main>
+  )
 }
 
 
