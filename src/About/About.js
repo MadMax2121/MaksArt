d79c7624541b757f457da03c146b357406a17d53
max_moskalenko <mmoskalenko21@gmail.com>
2024-01-09 06:41:18 -0500

    working on about us

--- src/About/About.js
+++ src/About/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./About.css"
+import {Link } from 'react-router-dom';
 
 function About() {
   return (
-    <main>
+    <main className='about'>
       <div className='image_banner'>
         <img src="/About/first_half.png" alt="" />
         <img src="/About/second_half.png" alt="" />
@@ -12,6 +13,18 @@ function About() {
           antique, vintage, and an extensive selection of contemporary art.
           No matter the genre or artist, our fine art collection offer a
           variety of styles, eras, and genre.</p>
+      </div>
+
+      <div className='about_us'>
+        <img src="/About/about_us.webp" alt="" />
+        <div className="description">
+          <div>About Us</div>
+          <p>We are a family-owned art gallery located in Framingham, MA.
+             Our showroom is open by appointment. Please feel free to contact
+             us for more information.</p>
+             <Link to = "/collection">Shop Art</Link>
+        </div>
+          
       </div>
     </main>
   )
