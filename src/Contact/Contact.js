24abb39008d6e11ee4031188ebcaedfe50e4594e
max_moskalenko <mmoskalenko21@gmail.com>
2024-01-15 14:15:13 -0500

    change

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
     <main className = "contact_main">
       <div className = "contact_header">
       <h1>Contact Us</h1>
-      <p>Input your contact informations and we will try to get back to you right away!</p>
+      <p>Input your contact information and we will try to get back to you right away!</p>
       </div>
       <div className="contact_name">
         <input placeholder='Names'></input>
