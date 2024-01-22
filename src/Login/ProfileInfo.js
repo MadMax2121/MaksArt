de00ec2e380d165518a00a2f1d16579386c6fba7
max_moskalenko <mmoskalenko21@gmail.com>
2024-01-22 17:39:53 -0500

    login done

--- src/Login/ProfileInfo.js
+++ src/Login/ProfileInfo.js
@@ -2,26 +2,41 @@ import React, { useContext } from 'react';
 //import { useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../Firebase';
-import { AuthContext} from './AuthContext'
+import { AuthContext } from './AuthContext'
+import "./ProfileInfo.css"
 
 function ProfileInfo() {
-  const { setCurrentUser, currentUser} = useContext(AuthContext); // If you're managing the current user in context
+    const { setCurrentUser, currentUser } = useContext(AuthContext); // If you're managing the current user in context
 
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      setCurrentUser(null); // Update the context if you're managing the current user
-    } catch (error) {
-      console.error("Error signing out: ", error);
-    }
-  };
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            setCurrentUser(null); // Update the context if you're managing the current user
+        } catch (error) {
+            console.error("Error signing out: ", error);
+        }
+    };
+    console.log("User ID: ", currentUser.uid);
+    console.log("Display Name: ", currentUser.displayName);
+    console.log("Email: ", currentUser.email);
+    console.log("Photo URL: ", currentUser.photoURL);
+    console.log("Email Verified: ", currentUser.emailVerified);
+    console.log("Provider Data: ", currentUser.providerData);
+    return (
+        <main style = {{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+            <h1 >Profile Information</h1>
+            <div className='profile_info_main'>
+                <img src={(currentUser.photoURL) ? currentUser.photoURL : "/login/no-profile-picture-15255.png"} alt=""/>
+                <div className='user_info'>
+                    <p><b>Name:</b> {currentUser.displayName}</p>
+                    <p><b>Email:</b> {currentUser.email} </p>
+                    <button onClick={handleLogout}>Logout</button>
 
-  return (
-    <main>
-    <h1>Hello, {currentUser.displayName}!</h1>
-    <button onClick={handleLogout}>Logout</button>
-    </main>
-  );
+                </div>
+                
+            </div>
+        </main>
+    );
 };
 
 export default ProfileInfo;
